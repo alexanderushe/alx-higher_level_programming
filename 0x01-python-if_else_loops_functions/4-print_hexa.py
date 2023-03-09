be99995d219fae6a3404cb4c6a2b1015afdfe23d
alexanderushe <alex.f.ushe@gmail.com>
2023-03-08 18:00:48 -0800

    hex

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 4-print_hexa.py
+# farai
+
+"""Print numbers 0 to 98 in decimal and hexadecimal"""
+for number in range(0, 98):
+    print("{} = {}".format(number, hex(number)))
