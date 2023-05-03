217b8b7ecb3a9464028efbce7cf7b79381755175
alexanderushe <alex.f.ushe@gmail.com>
2023-05-03 09:12:05 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-
 def read_file(filename=""):
-    """reads a file
-    """
+    """reads a file"""
     with open(filename, encoding='utf-8') as myFile:
         print(myFile.read(), end='')
