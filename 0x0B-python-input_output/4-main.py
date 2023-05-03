f5c679fe6a49c3984f0408bee7fe1e85b06d9811
alexanderushe <alex.f.ushe@gmail.com>
2023-05-03 11:50:39 +0200

    json from

--- 0x0B-python-input_output/4-main.py
+++ 0x0B-python-input_output/4-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-append_write = __import__('4-append_write').append_write
+append_write = __import__('4-from_json_string.py').append_write
 
 nb_characters_added = append_write("file_append.txt", "Holberton School is so cool!\n")
 print(nb_characters_added)
