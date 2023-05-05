fa8b642997ff2553b64086cf7bf1e7d5d512bec2
alexanderushe <alex.f.ushe@gmail.com>
2023-05-05 12:37:01 +0000

    11 student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
 
+"""Module that defines the class student
+"""
 
 class Student:
-    """student class for use
-    """
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+	"""class to create a student instance"""
 
-    def to_json(self):
-        """retrieves dict rep of Student in json form
-        """
-        return {key: value for (key, value) in self.__dict__.items()
-                if key in list(self.__dict__.keys())}
+	def __init__(self, first_name, last_name, age):
+		"""specail method to initialize"""
+		self.first_name = first_name
+		self.last_name = last_name
+		self.age = age
+
+	def to_json(self, attrs=None):
+		"""Method that returns directory description"""
+		obj = self.__dict__.copy()
+		if type(attrs) is list:
+
+			for item in attrs:
+				if type(item) is not str:
+					return obj
+
+			d_list = {}
+
+			for iatr in range(len(attrs)):
+				for satr in obj:
+					if attrs[iatr] == satr:
+						d_list[satr] = obj[satr]
+			return d_list
+		return obj
+	def reload_from_json(self, json):
+		"""replace all attributes of the student instance"""
+		for atr in json:
+			self.__dict__[atr] = json[atr]
