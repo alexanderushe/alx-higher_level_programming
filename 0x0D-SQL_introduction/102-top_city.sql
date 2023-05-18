6cfcecc64ae7e3e21cff658240884c3f6b733feb
alexanderushe <alex.f.ushe@gmail.com>
2023-05-18 23:53:51 +0200

    top cities

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- displya the top3 of cities temperature during july & august ODERED by temp
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
+
