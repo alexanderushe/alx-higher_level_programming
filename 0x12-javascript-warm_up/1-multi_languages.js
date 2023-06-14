600d525ae3ba591eb538524c3cbc7cdc11a83ebd
alexander ushe <alex.f.ushe@gmail.com>
2023-06-14 06:01:46 -0700

    multi languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 // Prints 3 lines
-
 const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
 const thirdLine = 'Javascript is amazing';
-console.log( firstLine + '\n' + secondLine + '\n' + thirdLine);
+console.log(firstLine + '\n' + secondLine + '\n' + thirdLine);
 
