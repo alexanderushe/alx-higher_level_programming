c085822694eb926ed9567d0120721773789258c8
alexanderushe <alex.f.ushe@gmail.com>
2023-07-04 06:51:28 -0700

    updates status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,9 @@
 - uses urllib package
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
