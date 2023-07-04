998b6a65b515ee17c973e3d585b0a2ee8a5ee5cf
alexanderushe <alex.f.ushe@gmail.com>
2023-07-04 03:30:07 -0700

    1body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#takes in URL, sends a GET responsse to the URL, displays the body of the response
-
+#  takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
